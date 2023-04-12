d4f786820103811fd6e0cf5aab627ad96f14ac15
heianadw <edwinchou2018@gmail.com>
2023-04-12 22:27:22 +0800

    add test case for quick sort

--- sort/main.go
+++ sort/main.go
@@ -8,4 +8,9 @@ func main() {
 	fmt.Println("unordered array:", bubbleArray)
 	BubbleSort(bubbleArray)
 	fmt.Println("sorted    array:", bubbleArray)
+	fmt.Println("------Quick sort------")
+	quickArray := []int{5, 4, 9, 8, 7, 6, 0, 1, 3, 2}
+	fmt.Println("unordered array:", quickArray)
+	QuickSort(quickArray)
+	fmt.Println("sorted    array:", quickArray)
 }
